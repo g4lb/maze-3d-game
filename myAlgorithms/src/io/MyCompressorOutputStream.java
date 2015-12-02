179639487dff8f2e7159c2ec9ddd6b3dc42ea4d1
galmalca <galmalca@10.0.0.8>
2015-12-02 19:44:32 +0200

    added io pockage with 1. MyComperessorOutputStream 2. MyDecompressorInputStream

---
+++ myAlgorithms/src/io/MyCompressorOutputStream.java
@@ -0,0 +1,14 @@
+package io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MyCompressorOutputStream extends OutputStream {
+
+	@Override
+	public void write(int b) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ myAlgorithms/src/io/MyDecompressorInputStream.java
@@ -0,0 +1,14 @@
+package io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyDecompressorInputStream extends InputStream {
+
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
