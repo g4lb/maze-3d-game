01b74c1ff83716ef41174ced87ac7ba50fb504f5
galmalca <galmalca@10.160.2.96>
2015-12-17 19:38:09 +0200

    javadoc

--- mvcByCLI/src/model/Model.java
+++ mvcByCLI/src/model/Model.java
@@ -41,16 +41,43 @@ public interface Model {
 	 * @return true/false
 	 */
 	boolean isInteger(String s);
-	/**
-	 * this method need to solve a maze2d by cross section
-	 * @param string
-	 */
 	void getCrossSection(ArrayList<String> string);
+	/**
+	 * this method save the maze into a file to the project dir and use decompresor to zip the file.
+	 * @param ArrayList<string> for the name of the maze and te new name for the file (by order).
+	 * @throws IOException
+	 */
 	void saveMaze(ArrayList<String> string) throws IOException;
+	/**
+	 * this method load the file of the maze and decompres him.
+	 * the method load the file from the project dir and becouse of this the player can save the maze
+	 * and play him any time.
+	 * @param ArrayList<string> for the name of the loaded maze and for the file name(by order)
+	 * @throws IOException
+	 */
 	void loadMaze(ArrayList<String> string) throws IOException;
+	/**
+	 * this method solve the maze by 1 of the algorithms Astar/BFS by the client request
+	 * @param ArrayList<string> for the name of the maze and for the algorithm.
+	 */
 	void solveMaze(ArrayList<String> string);
+	/**
+	 * this method shoe the solution of the solveMaze method.
+	 * @param ArrayList<string> for the name of the maze 
+	 */
 	void displaySolution(ArrayList<String> string);
+	/**
+	 * this method display file size.
+	 * @param ArrayList<string> for the file name 
+	 */
 	void displayFileSize(ArrayList<String> string);
+	/**
+	 * this method display size of maze.
+	 * @param ArrayList<string> for the maze name
+	 */
 	void displayMazeSize(ArrayList<String> string);
+	/**
+	 * this method make a safty exit from the program
+	 */
 	void stop();
 }
